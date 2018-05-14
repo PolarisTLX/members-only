6ed0c642d8b9752233e49fc0af198fbe7cfd13cd
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-05-14 11:38:04 -0400

    test login
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  fixtures :users
-  subject { users(:UserOne) }
-  # ":UserOne" is from the fixtures users.yml file
+  before do
+    Rails.application.load_seed
+  end
+
+  let(:user) { User.find_by(username: 'user1') }
 
   it "is valid with valid attributes" do
-    expect(subject).to be_valid
+    expect(user).to be_valid
   end
 
   it "is not valid without a username" do
-    subject.username = nil
-    expect(subject).to_not be_valid
+    user.username = nil
+    expect(user).to_not be_valid
   end
 
   it "is not valid without an password digest" do
-    subject.password_digest = nil
-    expect(subject).to_not be_valid
+    user.password_digest = nil
+    expect(user).to_not be_valid
   end
 end
